1a4d68217a4590cff7ab6da4e6204de8e9717bb1
shockdesign <rajesh@shockdesign.com.au>
2018-03-14 16:26:46 +1100

    See if this is the correct props

--- android/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
+++ android/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
@@ -5,12 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.wix.reactnativenotifications.core.notification.PushNotificationProps;
+import com.wix.reactnativenotifications.core.notifications.NotificationProps;
 
 public class NotificationIntentAdapter {
     private static final String PUSH_NOTIFICATION_EXTRA_NAME = "pushNotification";
 
-    public static PendingIntent createPendingNotificationIntent(Context appContext, Intent intent, PushNotificationProps notification) {
+    public static PendingIntent createPendingNotificationIntent(Context appContext, Intent intent, NotificationProps notification) {
         intent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
         return PendingIntent.getService(appContext, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT);
     }
